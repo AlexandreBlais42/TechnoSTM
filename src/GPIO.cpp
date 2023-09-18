70c4bc51cd8d17d9bffad37b1d7a86ca8da4663c
Alexandre Blais <>
2023-09-18 16:57:48 -0400

    Ajout d'un assert pour les pins

--- src/GPIO.cpp
+++ src/GPIO.cpp
@@ -4,5 +4,6 @@ GPIO::GPIO(){}
 
 void GPIO::setPin(const uint8_t pinNumber, const bool state){
   // À implémenter pour interfacer avec le raspberry pi
+  assert(pinNumber >=2 && pinNumber <= 27);
   std::cout << "GPIO::setPin appellé avec pinNumber = " << pinNumber << " state = " << state << std::endl;
 }
--- src/include/GPIO.h
+++ src/include/GPIO.h
@@ -3,11 +3,16 @@
 
 #include <iostream>
 #include <cstdint>
+#include <cassert>
 
 class GPIO{
 public:
   GPIO();
 
+  /** @brief Set une pin du GPIO du raspberry pi à state
+   *  @param pinNumber Le numéro de pin de GPIO
+   *  @param state État booléen de la pin à mettre
+   */
   static void setPin(const uint8_t pinNumber, const bool state);
 };
 
