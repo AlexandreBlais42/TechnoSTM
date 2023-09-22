df26cfa11d6e912651ddf23849241d6cb0ab93d9
Alexandre Blais <>
2023-09-22 15:29:01 -0400

    Ajout de la classe I2C

--- src/ADS1115.cpp
+++ src/ADS1115.cpp
@@ -9,7 +9,7 @@ uint16_t ADS1115::read(const uint8_t channel) const {
   assert(channel <= 3);
 
 #ifdef __arm__
-  // À implémenter
+  
   return 0;
 #else
   std::cout << "ADS1115::read() appellé avec channel = " << (uint16_t) channel << std::endl;
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
 #include "Plateforme.h"
 #include "Vector3D.h"
 #include "Utils.h"
+#include "I2C.h"
 
 int main(){
   Plateforme plateforme("/dev/ttyUSB0");
+  I2C conn(10);
 }
