1513185c63410a4d2726e70211bb6e732668caa0
Alexandre Blais <>
2023-10-03 13:59:14 -0400

    Implémentation de la classe Aiguille

--- src/Aiguille.cpp
+++ src/Aiguille.cpp
@@ -1,1 +1,7 @@
 #include "Aiguille.h"
+
+Aiguille::Aiguille(const uint8_t deviceAddr) : ADS1115(deviceAddr){}
+
+int16_t Aiguille::readVoltage(){
+  return read();
+}
--- src/include/Aiguille.h
+++ src/include/Aiguille.h
@@ -3,10 +3,21 @@
 
 #include <cstdint>
 
+#include "ADS1115.h"
+
 /** @brief Classe qui gère l'aiguille
  */
-class Aiguille {
+class Aiguille : private ADS1115 {
 public:
+  /** @brief Constructeur de la classe aiguille
+  *   @param ADCAddr
+  */
+  Aiguille(const uint8_t deviceAddr);
+
+  /** @brief Lit une valeur correspondant au courant sur l'aiguille
+  *   @return La valeur du courant lu
+  */
+  int16_t readVoltage();
 };
 
 #endif // AIGUILLE_H
