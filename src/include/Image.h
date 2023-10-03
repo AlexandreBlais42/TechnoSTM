f71e46c51fbbf8ec5c7400d4cf9df4ebae1612f6
Alexandre Blais <>
2023-10-03 15:26:47 -0400

    Ajout de la classe Image

---
+++ src/include/Image.h
@@ -0,0 +1,16 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+
+#include <iostream>
+#include <cstdint>
+// #include <gwyddion.h> ??????????
+
+/** @brief Classe pour l'image et le traitement de celle ci
+ */
+class Image{
+public:
+  Image();
+};
+
+#endif // IMAGE_H
