27bbcadcb83d3332c3989d44721c23b92f48ff66
Alexandre Blais <>
2023-11-07 21:16:56 -0500

    Ajout du choix du nombre de pas dans TestStepMotor.cpp

--- tests/TestStepMotor.cpp
+++ tests/TestStepMotor.cpp
@@ -5,11 +5,15 @@
 #include "StepMotor.h"
 
 int main(int argc, char *argv[]) {
+  int32_t numberOfSteps = -500; // Valeur par d�faut
+  if (argc >= 2){
+    numberOfSteps = std::stoi(argv[1]);
+  }
   std::array<uint8_t, 4> pins = {25, 8, 7, 1};
   StepMotor motor(0, pins);
   for (const auto pin : motor.pins) {
     std::cout << std::to_string(pin) << std::endl;
   }
-  motor.setPositionRelative(-500);
+  motor.setPositionRelative(numberOfSteps);
   return 0;
 }
